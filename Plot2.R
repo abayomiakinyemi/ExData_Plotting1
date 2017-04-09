4ce6d94ef77702f24fdea7d9d0176fa2843f9d03
Akinyemi Abayomi Tosin <abayomiakinyemi@yahoo.com>
2017-04-09 23:44:34 +0100

    Plot2

--- Plot2.R
+++ Plot2.R
@@ -1,14 +1,14 @@
-if(!file.exists("./Data/data.zip")){
+if(!file.exists("Data2/data.zip")){
         dir.create("Data")
         # Download and extract zipped data file
         fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-        download.file(fileUrl, destfile="./Data/data.zip")
-        unzip(zipfile="./Data/data.zip", exdir="./Data")
+        download.file(fileUrl, destfile="Data2/data.zip")
+        unzip(zipfile="Data2/data.zip", exdir="./Data2")
 }
 
 # File unzipped as "household_power_consumption.txt" textfile
 # Read a subset of the datafile
-filePath <- "./Data/household_power_consumption.txt"
+filePath <- "Data2/household_power_consumption.txt"
 data <- read.csv(filePath, skip=66637, nrows=2880, na.strings = "?", header=FALSE, sep=";")
 
 # Read the variable names
@@ -23,5 +23,5 @@ data$DateTime <- as.POSIXct(paste(data$Date, data$Time, sep=" "), format="%d/%m/
 plot(data$DateTime, data$Global_active_power, type="l", col="black", xlab="", ylab="Global Active Power (kilowatts)", main="")
  
 ## Saving "Plot2.png" to file
-dev.copy(png, file="./ExData_Plotting1/plot2.png", height=480, width=480)
+dev.copy(png, file="Data2/plot2.png", height=480, width=480)
 dev.off() 
